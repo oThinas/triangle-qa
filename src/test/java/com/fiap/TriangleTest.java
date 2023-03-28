dc9a6e77a11fa73554ef03bd7dd013892c26a637
oThinas <thiagomartinsprado@gmail.com>
2023-03-28 15:05:04 -0300

    feat: 100% de cobertura

--- src/test/java/com/fiap/TriangleTest.java
+++ src/test/java/com/fiap/TriangleTest.java
@@ -14,8 +14,20 @@ public class TriangleTest {
   }
 
   @Test
-  public void testIsosceles() {
+  public void testIsosceles1() {
     Triangle triangle = new Triangle(3, 3, 5.8);
+    assertEquals("Isósceles", triangle.getType());
+  }
+
+  @Test
+  public void testIsosceles2() {
+    Triangle triangle = new Triangle(3, 5, 5);
+    assertEquals("Isósceles", triangle.getType());
+  }
+
+  @Test
+  public void testIsosceles3() {
+    Triangle triangle = new Triangle(3, 5, 3);
     assertEquals("Isósceles", triangle.getType());
   }
 
@@ -36,9 +48,49 @@ public class TriangleTest {
   }
 
   @Test
-  public void testInvalidSideLenght() {
+  public void testZeroSide1() {
+    try {
+      new Triangle(0, 3, 3);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException exception) {
+      assertEquals("Os lados do triângulo devem ser maiores que zero.", exception.getMessage());
+    }
+  }
+
+  @Test
+  public void testZeroSide2() {
+    try {
+      new Triangle(5, 8, 0);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException exception) {
+      assertEquals("Os lados do triângulo devem ser maiores que zero.", exception.getMessage());
+    }
+  }
+
+  @Test
+  public void testInvalidSideLenght1() {
     try {
       new Triangle(3, 3, 9);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException exception) {
+      assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", exception.getMessage());
+    }
+  }
+
+  @Test
+  public void testInvalidSideLenght2() {
+    try {
+      new Triangle(6, 3, 3);
+      fail("Expected IllegalArgumentException");
+    } catch (IllegalArgumentException exception) {
+      assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", exception.getMessage());
+    }
+  }
+
+  @Test
+  public void testInvalidSideLenght3() {
+    try {
+      new Triangle(5, 14, 5.5);
       fail("Expected IllegalArgumentException");
     } catch (IllegalArgumentException exception) {
       assertEquals("A soma de dois lados deve ser maior que o terceiro lado.", exception.getMessage());
@@ -50,11 +102,11 @@ public class TriangleTest {
     Triangle triangle = new Triangle(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
     assertEquals("Equilátero", triangle.getType());
   }
-  
+
   @Test
   public void testSmallNumbers() {
     Triangle triangle = new Triangle(Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE);
     assertEquals("Equilátero", triangle.getType());
   }
-  
+
 }
